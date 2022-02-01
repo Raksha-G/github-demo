f305196ccc75859399421aff1968819cf406df77
Raksha-G <98821620+Raksha-G@users.noreply.github.com>
2022-02-01 19:25:50 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,20 @@
+#Add implementation
+def add(x,y):
+	return x+y;
+
+#subtract implementation
+def subtract(x,y):
+	return x-y on master branch
+
+#multiply implementation
+def multiply(x,y):
+	return x*y on bug123
+
+#divide implementation
+def divide(x,y):
+
+	if y==0		#implementation for master branch
+		return divide by 0 error
+	else
+		return x/y
+
